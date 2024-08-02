94a6d5df1f72bc68f06321bf4a23400113471d51
Snesnopic <snesnopic@gmail.com>
2024-08-02 10:20:22 +0200

    Yet more error logging

--- src/controllers/AnswerController.js
+++ src/controllers/AnswerController.js
@@ -9,7 +9,7 @@ class AnswerController {
       let decoded = null
       try {
          decoded = jwt.verify(token, process.env.JWT_SECRET);
-      } catch {
+      } catch (error){
         // This probably just means that the user isn't logged in, and should answer as Anonymous
         console.log("Error:".concat(error))
       }
